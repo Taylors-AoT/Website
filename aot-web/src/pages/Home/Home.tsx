b784f2466835ed104f5bf83ca1df319faf380d0e
Reo <tmreo123@gmail.com>
2026-06-03 21:14:45 +0800

    FIle extension changes

--- aot-web/src/pages/Home/Home.tsx
+++ aot-web/src/pages/Home/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
       <section className={styles.heroSection}>
         {/* Background Image */}
         <img 
-            src="/excobod/EXCOBOD Group.JPG" 
+            src="/excobod/EXCOBOD Group.jpg" 
             alt="Agents of Tech Team" 
             className={styles.heroBackground} 
         />
